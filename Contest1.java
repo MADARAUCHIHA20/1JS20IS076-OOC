78b2170019b8598e8bfd9ebd0200352678e738d9
Pratik KM <93049293+MADARAUCHIHA20@users.noreply.github.com>
2022-09-06 14:21:31 +0530

    Update Contest1.java

--- Contest1.java
+++ Contest1.java
@@ -35,7 +35,7 @@ public class Contest1 {
   }
 
   public static void main(String[] args) {
-    String str = "My name is *Ashok Kumar Gowda* I have 12+ years of experience I work for *IBM India Pvt Ltd* ";
+    String str = "My name is *Pratik K M ,I am Studying in JSSATEB* ";
 
     List < String > result = splitString(str, '*');
 
